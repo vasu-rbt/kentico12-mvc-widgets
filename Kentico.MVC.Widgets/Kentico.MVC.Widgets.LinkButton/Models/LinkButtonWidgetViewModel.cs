07790ccf60e3b849f053201fee48268e584177ef
vasu <vasu.yerramsetti>
2019-08-28 14:32:21 +0530

    Initial commit

---
+++ Kentico.MVC.Widgets/Kentico.MVC.Widgets.LinkButton/Models/LinkButtonWidgetViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Kentico.MVC.Widgets
+{
+    public class LinkButtonWidgetViewModel
+    {
+        public string LinkText { get; set; }
+        public string LinkCSSClass { get; set; }
+        public string LinkURL { get; set; }
+        public string LinkTarget { get; set; }
+    }
+}
