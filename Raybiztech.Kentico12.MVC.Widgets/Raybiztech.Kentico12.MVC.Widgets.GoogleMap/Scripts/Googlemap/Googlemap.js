40b1ed3b25bdde1d10b1ff38a3eb357dd9b0aa41
Srikanth <srikanth.nasa@raybiztech.com>
2019-12-17 13:22:35 +0530

    issue fixed related to multipple googlemap widget added in page

--- Raybiztech.Kentico12.MVC.Widgets/Raybiztech.Kentico12.MVC.Widgets.GoogleMap/Scripts/Googlemap/Googlemap.js
+++ Raybiztech.Kentico12.MVC.Widgets/Raybiztech.Kentico12.MVC.Widgets.GoogleMap/Scripts/Googlemap/Googlemap.js
@@ -1,23 +1,18 @@
 ﻿function initialize() {
-    var latitude = document.getElementById('latitude').value;
-    var logitude = document.getElementById('logitude').value;
-    var list = document.getElementById("googlemap");
-    var js = document.createElement('div');
-    js.id = new Date().getTime().toString(16);
-    js.setAttribute('style', "width: 100%;height: 400px;")
-    list.after(js);
-    var mapOptions = {
-        center: new google.maps.LatLng(latitude, logitude),
-        zoom: 4,
-        mapTypeId: 'roadmap',
-    };
-    var map = new google.maps.Map(document.getElementById(js.id), mapOptions);
-    var bounds = new google.maps.LatLngBounds();
-    var latlng = new google.maps.LatLng(latitude, logitude);
-    var marker = new google.maps.Marker({
-        map: map,
-        position: latlng,
-        title: 'Google Map'
+    $('.googlemap_class').each(function () {
+        var mapOptions = {
+            center: new google.maps.LatLng(this.attributes['data-latitude'].value, this.attributes['data-longitude'].value),
+            zoom: 4,
+            mapTypeId: 'roadmap',
+        };
+        var map = new google.maps.Map(document.getElementById(this.attributes['id'].value), mapOptions);
+        var bounds = new google.maps.LatLngBounds();
+        var latlng = new google.maps.LatLng(this.attributes['data-latitude'].value, this.attributes['data-longitude'].value);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: latlng,
+            title: 'Google Map'
+        });
+        bounds.extend(latlng);
     });
-    bounds.extend(latlng);
 }
